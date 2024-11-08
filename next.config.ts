be9005e0bb7326a414f5570ca70c3cb582322ab9
zmariann <zaszlosmariann@gmail.com>
2024-11-08 13:35:18 +0100

    Chapter 10 | Partial Prerendering Redo

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  experimental: {
-    ppr: "incremental",
-  },
+  /* config options here */
 };
 
 export default nextConfig;
